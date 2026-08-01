80eeeaebb3b226cba81630c0d465cc5f3fe1868c
Shourya Sharma <shouryasharma1203@gmail.com>
2026-08-02 02:05:47 +0530

    updated departmentsList

--- FRONTEND/src/constants/academic.js
+++ FRONTEND/src/constants/academic.js
@@ -1,8 +1,13 @@
 export const departmentsList = [
   { id: 'computer science', name: 'Computer Science' },
-  { id: 'electronics', name: 'Electronics' },
+  { id: 'electronics', name: 'Electronics and Communication Engineering' },
   { id: 'information technology', name: 'Information Technology' },
-  { id: 'mechanical', name: 'Mechanical' },
+  { id: 'mechanical', name: 'Mechanical Engineering' },
+  { id: 'civil', name: 'Civil Engineering' },
+  { id: 'electrical', name: 'Electrical Engineering' },
+  { id: 'chemical', name: 'Chemical Engineering' },
+  { id: 'metallurgy', name: 'Metallurgy' },
+  { id: 'mining', name: 'Mining' },
 ];
 export const examTypes = ['Mid-Sem', 'End-Sem', 'Quiz', 'Other'];
 
