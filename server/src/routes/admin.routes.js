6a35f6265241d9616003b9e69f975e9c493c25cc
Shourya Sharma <shouryasharma1203@gmail.com>
2026-08-31 21:23:22 +0530

    removed rate limited

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -2,12 +2,11 @@ import { Router } from 'express';
 import { getAdminDashboard, uploadResource , deleteResource, verifiedAdminToken} from '../controllers/admin.controller.js';
 import {verifyAdminToken} from '../middlewares/tokenAuth.middleware.js';
 import upload from '../middlewares/multer.middleware.js';
-import { securityLimiter } from '../middlewares/security.middleware.js';
 
 const router = Router();
 
 router.get('/dashboard', verifyAdminToken, getAdminDashboard);
-router.post('/upload', securityLimiter, verifyAdminToken, upload.single('resourceFile'), uploadResource);
+router.post('/upload',  verifyAdminToken, upload.single('resourceFile'), uploadResource);
 router.post('/verify', verifyAdminToken,verifiedAdminToken);
 router.delete('/resources/:id', verifyAdminToken, deleteResource);
 export default router;
